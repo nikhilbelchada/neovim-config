6f55098b112efc29ae699065e6f7ec16a7d09fb1
Nikhil <nick.gokuz@gmail.com>
2025-09-19 15:38:49 +0530

    Updated key maps

--- lua/nikhil/lazy/fterm.lua
+++ lua/nikhil/lazy/fterm.lua
@@ -1,5 +1,5 @@
-vim.keymap.set('n', '<C-i>', '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '<C-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('n', '<C-t>', '<CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('t', '<C-t>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 
 return {
   'numToStr/FTerm.nvim'
--- lua/nikhil/set.lua
+++ lua/nikhil/set.lua
@@ -23,3 +23,8 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+-- Tabs
+vim.keymap.set('n', '<C-n>', ":tabnew<CR>", { desc = '[T]ab New' })
+vim.keymap.set('n', '<C-o>', ":tabnext<CR>", { desc = '[T]ab Next' })
+vim.keymap.set('n', '<C-i>', ":tabprevious<CR>", { desc = '[T]ab Prev' })
