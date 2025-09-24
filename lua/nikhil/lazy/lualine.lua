cdde0b837d5c3b6e253e369affb0de85b89a03ce
Nikhil <nick.gokuz@gmail.com>
2025-09-24 22:17:03 +0530

    Micro Python fix

--- lua/nikhil/lazy/lualine.lua
+++ lua/nikhil/lazy/lualine.lua
@@ -2,6 +2,15 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require("lualine").setup()
+    require("lualine").setup({
+      sections = {
+        lualine_b = {
+          {
+            require("micropython_nvim").statusline,
+            cond = package.loaded["micropython_nvim"] and require("micropython_nvim").exists,
+          },
+        }
+      }
+    })
   end
 }
--- lua/nikhil/lazy/micropython.lua
+++ lua/nikhil/lazy/micropython.lua
@@ -3,17 +3,5 @@ vim.keymap.set("n", "<leader>mr", require("micropython_nvim").run)
 
 return {
   "jim-at-jibba/micropython.nvim",
-  dependencies = { "akinsho/toggleterm.nvim", "stevearc/dressing.nvim" },
-  config = function() 
-    require("lualine").setup({
-      sections = {
-        lualine_b = {
-          {
-            require("micropython_nvim").statusline,
-            cond = package.loaded["micropython_nvim"] and require("micropython_nvim").exists,
-          },
-        }
-      }
-    })
-  end
+  dependencies = { "akinsho/toggleterm.nvim", "stevearc/dressing.nvim" }
 }
